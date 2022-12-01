09fd898eb05308f14feeefbd7f7ad20c53dc9d6f
athomas-wtv <athomas.wtv@gmail.com>
2022-11-30 21:34:44 -0500

    Added summary and buttons to the first two pages

--- app/js/controllers/formController.js
+++ app/js/controllers/formController.js
@@ -1,16 +1,32 @@
 (function() {
 
   const formController = function($scope, $routeParams, profilesFactory, $location) {
+
+    $scope.profile = {
+      firstName: "something",
+      lastName: "",
+      dob: "",
+      gender: "",
+      sport: "",
+      location: "",
+      team: "",
+      about: ""
+    }
+
     if ($routeParams.id) {
       function init() {
-        profilesFactory.getProfiles().then(athleteData => {
-          $scope.athleteData = athleteData;
+        profilesFactory.getProfile($routeParams.id).then(profile => {
+          $scope.profile = profile;
         });
       }
       init();
     }
 
-    $scope.navigateTo = (route) => $location.path(`./${route}`);
+    $scope.navigateTo = (route) => $location.path(`/${route}`);
+
+    $scope.addProfile = (profile) => {
+      console.log(profile);
+    }
 
     const formSubmit = () => {
       profilesFactory.updateProfile($scope.athleteData).then(() => {
